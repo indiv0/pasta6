90bcc7b9bea4411951982978d568c0c616e9026d
Nikita Pekin <contact@nikitapek.in>
2020-08-11 23:50:46 -0400

    impr: Echo POST requests
    Replace the hello world example with a warp server that echoes POST
    requests. The `/paste` endpoint accepts arbitrary bytes, up to a length
    limit of 16kb. For example:
    
        $ curl -XPOST -dhello localhost:3030/paste
        bytes = b"hello"

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let paste = warp::post()
+        .and(warp::path("paste"))
+        // Only accept bodies smaller than 16kb
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::bytes())
+        .map(|bytes| {
+            format!("bytes = {:?}", bytes)
+        });
 
-    warp::serve(hello)
+    warp::serve(paste)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
