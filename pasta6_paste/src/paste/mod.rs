ce45232f72e56e12dc153b6189c3ab56a9d7ae6b
Nikita Pekin <nikita@frecency.com>
2021-12-29 18:27:06 -0500

    chore: Reset repository

--- pasta6_paste/src/paste/mod.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) use db::{init_db, Hash};
-pub(crate) use filter::{create_paste, get_paste};
-
-mod db;
-mod filter;
-mod models;
--- pasta6_util/src/bin/generate_key.rs
+++
@@ -1,8 +0,0 @@
-use base64::URL_SAFE;
-use sodiumoxide::crypto::aead::xchacha20poly1305_ietf::gen_key;
-
-fn main() {
-    let secret_key = gen_key();
-    let encoded = base64::encode_config(secret_key.as_ref(), URL_SAFE);
-    println!("{}", encoded);
-}
