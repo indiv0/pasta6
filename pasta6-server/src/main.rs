727f484c4105f7ef66891016398f7faf10b33973
Nikita Pekin <nikita@frecency.com>
2021-12-30 20:25:24 -0500

    feat: Add example POST endpoint with sled DB

--- pasta6-server/src/main.rs
+++ pasta6-server/src/main.rs
@@ -1,3 +1,5 @@
+use hyper::Body;
+use hyper::Request;
 use monoio::utils;
 use monoio::RuntimeBuilder;
 
@@ -14,15 +16,22 @@ cores: {:?}",
         ADDR, CORES
     );
 
+    let db = sled::open("database").expect("error opening pasta6.db");
+
     let mut threads = Vec::new();
     for cpu in CORES {
+        let db = db.clone();
         let handle = thread::spawn(move || {
             utils::bind_to_cpu_set(Some(*cpu)).expect("error binding to CPU");
             let mut rt = RuntimeBuilder::new()
                 .with_entries(32768)
                 .build()
                 .expect("error building monoio runtime");
-            rt.block_on(pasta6::serve(ADDR, pasta6::handler))
+            let handler = move |req: Request<Body>| {
+                let db = db.clone();
+                async move { pasta6::handler(&db, req).await }
+            };
+            rt.block_on(pasta6::serve(ADDR, handler))
                 .expect("error serving HTTP requests");
         });
         threads.push(handle);
--- pasta6/src/lib.rs
+++ pasta6/src/lib.rs
@@ -1,10 +1,11 @@
 use hyper::{
-    rt::Executor, server::conn::Http, service, Body, Method, Request, Response, StatusCode,
+    body, rt::Executor, server::conn::Http, service, Body, Method, Request, Response, StatusCode,
 };
 use monoio::net::TcpListener;
 use monoio_compat::TcpStreamCompat;
+use sled::Db;
 
-use std::{convert::Infallible, error::Error, future::Future, io, net::ToSocketAddrs};
+use std::{convert::Infallible, error::Error, future::Future, io, net::ToSocketAddrs, str};
 
 #[derive(Clone)]
 pub struct HyperExecutor;
@@ -22,7 +23,7 @@ where
 pub async fn serve<A, S, F, R>(addr: A, service: S) -> io::Result<()>
 where
     A: ToSocketAddrs,
-    S: FnMut(Request<Body>) -> F + 'static + Copy,
+    S: FnMut(Request<Body>) -> F + 'static + Clone,
     F: Future<Output = Result<Response<Body>, R>> + 'static,
     R: Error + 'static + Send + Sync,
 {
@@ -33,17 +34,88 @@ where
         monoio::spawn(
             Http::new()
                 .with_executor(HyperExecutor)
-                .serve_connection(stream, service::service_fn(service)),
+                .serve_connection(stream, service::service_fn(service.clone())),
         );
     }
 }
 
-pub async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+pub async fn handler(db: &Db, req: Request<Body>) -> Result<Response<Body>, Infallible> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(Body::from("Hello, world!"))),
+        (&Method::GET, "/todo") => get_todo(req),
+        (&Method::POST, "/todo") => post_todo(db, req).await,
         _ => Ok(Response::builder()
             .status(StatusCode::NOT_FOUND)
             .body(Body::from("404 not found"))
             .unwrap()),
     }
+}
+
+pub fn get_todo(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let response = Response::new(Body::from(
+        "\
+<html>\
+<head></head>\
+<body>\
+  <form method=\"post\">\
+    <label for=\"content\">TODO:</label>\
+    <input type=\"text\" name=\"content\" id=\"content\" required>\
+  </form>\
+</body>\
+</html>\
+",
+    ));
+    Ok(response)
+}
+
+pub async fn post_todo(db: &Db, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    use rand::Rng;
+
+    let body = match body::to_bytes(req.into_body()).await {
+        Ok(b) => b,
+        Err(_e) => todo!(),
+    };
+    let string = match str::from_utf8(&body) {
+        Ok(s) => s,
+        Err(_e) => todo!(),
+    };
+    let mut pair = string.split('=');
+    // FIXME: handle missing parts.
+    let (key, value) = (pair.next().unwrap(), pair.next().unwrap());
+    // FIXME: don't assert here?
+    assert_eq!(key, "content");
+
+    // TODO: use snowflakes instead of random keys & remove the rand dep.
+    let key: u64 = rand::thread_rng().gen();
+
+    // TODO: don't unwrap here
+    db.insert(key.to_be_bytes(), value).unwrap();
+
+    // TODO: what's the right way to iterate over all values?
+    let todos = db.range(0u64.to_be_bytes()..).map(|e| e.map(|(_, v)| v));
+    // TODO: do this more efficiently with templates.
+    let todos: String = todos
+        .map(|v| {
+            format!("<li>{}</li>", unsafe {
+                str::from_utf8_unchecked(&v.unwrap())
+            })
+        })
+        .collect();
+
+    let response = Response::new(Body::from(format!(
+        "\
+<html>\
+<head></head>\
+<body>\
+  <form method=\"post\">\
+    <label for=\"content\">TODO:</label>\
+    <input type=\"text\" name=\"content\" id=\"content\" required>\
+  </form>\
+  <ul>{}<ul>\
+</body>\
+</html>\
+",
+        todos
+    )));
+    Ok(response)
 }
