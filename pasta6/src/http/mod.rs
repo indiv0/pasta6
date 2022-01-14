e163b055e375c8985b087ee6627b867bf46965f1
Nikita Pekin <nikita@frecency.com>
2022-01-14 15:16:04 -0500

    impr: Keep connection open for multiple requests

--- pasta6/src/http/mod.rs
+++ pasta6/src/http/mod.rs
@@ -95,130 +95,136 @@ where
     H: Handler,
 {
     tracing::trace!("server handling connection");
-    // Allocate a buffer to store request data.
-    // TODO: re-use this buffer between requests.
-    // TODO: allow non-contiguous buffers to allow re-allocation.
-    // TODO: add a limit to the size of a single HTTP header.
-    // TODO: add a limit to the total size of the request head.
-    // TODO: shrink this buffer after the request is processed?
-    let mut buf = vec![0; INIT_REQUEST_BUFFER_SIZE];
-    // Read as much data as possible from the TCP stream into the buffer.
-    tracing::trace!("server reading stream");
-    let mut bytes_read = 0;
-    let mut reached_eof = false;
-    let request = loop {
-        // If there is no remaining space in the buffer to read into, then
-        // we need to grow the buffer.
-        // TODO: grow the buffer in powers of two to perform O(log n)
-        //   allocations rather than O(n) allocations.
-        if bytes_read >= buf.len() {
-            debug_assert!(bytes_read == buf.len());
-            const EMPTY_BUFFER: [u8; INIT_REQUEST_BUFFER_SIZE] = [0; INIT_REQUEST_BUFFER_SIZE];
-            buf.extend_from_slice(&EMPTY_BUFFER);
-        }
+    'outer: loop {
+        // Allocate a buffer to store request data.
+        // TODO: re-use this buffer between requests.
+        // TODO: allow non-contiguous buffers to allow re-allocation.
+        // TODO: add a limit to the size of a single HTTP header.
+        // TODO: add a limit to the total size of the request head.
+        // TODO: shrink this buffer after the request is processed?
+        let mut buf = vec![0; INIT_REQUEST_BUFFER_SIZE];
+        // Read as much data as possible from the TCP stream into the buffer.
+        tracing::trace!("server reading stream");
+        let mut bytes_read = 0;
+        let mut reached_eof = false;
+        let request = loop {
+            // If there is no remaining space in the buffer to read into, then
+            // we need to grow the buffer.
+            // TODO: grow the buffer in powers of two to perform O(log n)
+            //   allocations rather than O(n) allocations.
+            if bytes_read >= buf.len() {
+                debug_assert!(bytes_read == buf.len());
+                const EMPTY_BUFFER: [u8; INIT_REQUEST_BUFFER_SIZE] = [0; INIT_REQUEST_BUFFER_SIZE];
+                buf.extend_from_slice(&EMPTY_BUFFER);
+            }
 
-        bytes_read += match tcp_stream.read(&mut buf[bytes_read..]) {
-            Ok(0) => {
-                tracing::trace!("reached EOF");
-                reached_eof = true;
-                bytes_read
-            }
-            Ok(bytes_read) => {
-                tracing::trace!("server bytes read: {}", bytes_read);
-                bytes_read
-            }
+            bytes_read += match tcp_stream.read(&mut buf[bytes_read..]) {
+                Ok(0) => {
+                    tracing::trace!("reached EOF");
+                    // If we've reached EOF and there are no unparsed bytes,
+                    // then the client has closed the connection.
+                    if bytes_read == 0 {
+                        return Ok(());
+                    }
+                    reached_eof = true;
+                    bytes_read
+                }
+                Ok(bytes_read) => {
+                    tracing::trace!("server bytes read: {}", bytes_read);
+                    bytes_read
+                }
+                Err(e) => {
+                    tracing::error!("read error: {}", e);
+                    panic!();
+                }
+            };
+            // Parse the data into an HTTP request.
+            tracing::trace!(
+                "server parsing request: {}",
+                String::from_utf8_lossy(&buf[..bytes_read])
+            );
+            match Request::parse(&buf[..bytes_read]) {
+                ParseResult::Ok(request) => break request,
+                ParseResult::Partial => {
+                    if !reached_eof {
+                        continue;
+                    } else {
+                        // TODO: is just returning in the event of an unparsable HTTP
+                        //   request head and an unexpected EOF the correct thing to
+                        //   do?
+                        return Err(ConnectionError {
+                            _kind: ConnectionErrorKind::UnexpectedEof,
+                        });
+                    }
+                }
+                ParseResult::Error(e) => match e {
+                    // RFC 6585 section 5:
+                    // > The 431 status code indicates that the server is
+                    // > unwilling to process the request because its header
+                    // > fields are too large. The request MAY be resubmitted
+                    // > after reducing the size of the request header fields.
+                    // >
+                    // > It can be used both when the set of request header
+                    // > fields in total is too large, and when a single header
+                    // > field is at fault. In the latter case, the response
+                    // > representation SHOULD specify which header field was too
+                    // > large.
+                    httparse::Error::TooManyHeaders => {
+                        let response = Response::from_static(431, "");
+                        tracing::trace!("server writing response");
+                        match connection::write_response(&mut tcp_stream, response) {
+                            Ok(()) => {}
+                            Err(e) => {
+                                tracing::error!("write error: {}", e);
+                                panic!()
+                            }
+                        }
+                        tracing::trace!("server flushing response");
+                        match tcp_stream.flush() {
+                            Ok(()) => {}
+                            Err(e) => {
+                                tracing::error!("flush error: {}", e);
+                                panic!();
+                            }
+                        }
+                        break 'outer;
+                    }
+                    _ => {
+                        tracing::error!("parse error: {}", e);
+                        panic!()
+                    }
+                },
+            };
+        };
+        tracing::trace!("server total bytes read: {}", bytes_read);
+        // Invoke the provided handler function to process the request.
+        let response = handler.handle(&request);
+        // TODO: what's the proper behaviour if the handler defined these headers?
+        if response.headers().get("content-length").is_some() {
+            tracing::error!("unexpected header: content-length");
+            panic!()
+        }
+        if response.headers().get("date").is_some() {
+            tracing::error!("unexpected header: date");
+            panic!()
+        }
+        // Send the response back to the client.
+        // TODO: investigate perf of multiple `write_all` vs single `write!`.
+        tracing::trace!("server writing response");
+        match connection::write_response(&mut tcp_stream, response) {
+            Ok(()) => {}
             Err(e) => {
-                tracing::error!("read error: {}", e);
+                tracing::error!("write error: {}", e);
+                return Ok(());
+            }
+        }
+        tracing::trace!("server flushing response");
+        match tcp_stream.flush() {
+            Ok(()) => {}
+            Err(e) => {
+                tracing::error!("flush error: {}", e);
                 panic!();
             }
-        };
-        // Parse the data into an HTTP request.
-        tracing::trace!(
-            "server parsing request: {}",
-            String::from_utf8_lossy(&buf[..bytes_read])
-        );
-        match Request::parse(&buf[..bytes_read]) {
-            ParseResult::Ok(request) => break request,
-            ParseResult::Partial => {
-                if !reached_eof {
-                    continue;
-                } else {
-                    // TODO: is just returning in the event of an unparsable HTTP
-                    //   request head and an unexpected EOF the correct thing to
-                    //   do?
-                    return Err(ConnectionError {
-                        _kind: ConnectionErrorKind::UnexpectedEof,
-                    });
-                }
-            }
-            ParseResult::Error(e) => match e {
-                // RFC 6585 section 5:
-                // > The 431 status code indicates that the server is
-                // > unwilling to process the request because its header
-                // > fields are too large. The request MAY be resubmitted
-                // > after reducing the size of the request header fields.
-                // >
-                // > It can be used both when the set of request header
-                // > fields in total is too large, and when a single header
-                // > field is at fault. In the latter case, the response
-                // > representation SHOULD specify which header field was too
-                // > large.
-                httparse::Error::TooManyHeaders => {
-                    let response = Response::from_static(431, "");
-                    tracing::trace!("server writing response");
-                    match connection::write_response(&mut tcp_stream, response) {
-                        Ok(()) => {}
-                        Err(e) => {
-                            tracing::error!("write error: {}", e);
-                            panic!()
-                        }
-                    }
-                    tracing::trace!("server flushing response");
-                    match tcp_stream.flush() {
-                        Ok(()) => {}
-                        Err(e) => {
-                            tracing::error!("flush error: {}", e);
-                            panic!();
-                        }
-                    }
-                    tracing::trace!("server closing connection");
-                    return Ok(());
-                }
-                _ => {
-                    tracing::error!("parse error: {}", e);
-                    panic!()
-                }
-            },
-        };
-    };
-    tracing::trace!("server total bytes read: {}", bytes_read);
-    // Invoke the provided handler function to process the request.
-    let response = handler.handle(&request);
-    // TODO: what's the proper behaviour if the handler defined these headers?
-    if response.headers().get("content-length").is_some() {
-        tracing::error!("unexpected header: content-length");
-        panic!()
-    }
-    if response.headers().get("date").is_some() {
-        tracing::error!("unexpected header: date");
-        panic!()
-    }
-    // Send the response back to the client.
-    // TODO: investigate perf of multiple `write_all` vs single `write!`.
-    tracing::trace!("server writing response");
-    match connection::write_response(&mut tcp_stream, response) {
-        Ok(()) => {}
-        Err(e) => {
-            tracing::error!("write error: {}", e);
-            panic!();
-        }
-    }
-    tracing::trace!("server flushing response");
-    match tcp_stream.flush() {
-        Ok(()) => {}
-        Err(e) => {
-            tracing::error!("flush error: {}", e);
-            panic!();
         }
     }
     tracing::trace!("server closing connection");
